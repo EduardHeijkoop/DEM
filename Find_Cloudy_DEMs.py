f908cc485a8dd85aa90d5c9dc42fa91e8a00e089
Eduard Heijkoop <Eduard.Heijkoop@gmail.com>
2023-06-19 11:05:07 -0600

    Now detects clouds that were already removed too.

--- Find_Cloudy_DEMs.py
+++ Find_Cloudy_DEMs.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import geopandas as gpd
+import shapely
 import subprocess
 import argparse
 import configparser
@@ -10,7 +12,7 @@ from osgeo import gdal,gdalconst,osr
 
 
 from Global_DEMs import download_srtm,download_aster,download_copernicus
-from dem_utils import get_strip_list,get_list_extents,get_strip_extents,resample_raster
+from dem_utils import get_strip_list,get_list_extents,get_strip_extents,resample_raster,get_strip_shp,raster_to_geotiff
 
 
 
@@ -26,14 +28,14 @@ def main():
     parser.add_argument('--loc_name',default=None,help='Location name.')
     parser.add_argument('--a_priori',default='copernicus',help='A priori DEM to use.',choices=['srtm','aster','copernicus'])
     parser.add_argument('--coastline',default=config.get('GENERAL_PATHS','osm_shp_file'),help='Path to coastline shapefile')
-    parser.add_argument('--pct_threshold',default=0.05,type=float,help='Threshold exceedance value.')
+    parser.add_argument('--pct_threshold',default=config.get('MOSAIC_CONSTANTS','STRIP_CLOUD_THRESHOLD'),type=float,help='Threshold exceedance value.')
     parser.add_argument('--vertical_threshold',default=50,type=float,help='Vertical threshold for exceedance.')
     parser.add_argument('--machine',default='t',help='Machine to run on.',choices=['t','b','local'])
     parser.add_argument('--dir_structure',default='sealevel',help='Directory structure of input strips',choices=['sealevel','simple'])
     parser.add_argument('--cpus',help='Number of CPUs to use.',default=1,type=int)
     parser.add_argument('--keep_diff',help='Keep DEM differences.',action='store_true')
     parser.add_argument('--find_water',default=False,help='Find percentage of water in DEM too.',action='store_true')
-    parser.add_argument('--water_threshold',default=0.3,type=float,help='Water percentage threshold.')
+    parser.add_argument('--water_threshold',default=config.get('MOSAIC_CONSTANTS','STRIP_WATER_THRESHOLD'),type=float,help='Water percentage threshold.')
     parser.add_argument('--quiet',default=False,help='Suppress output.',action='store_true')
 
     args = parser.parse_args()
@@ -138,7 +140,11 @@ def main():
         a_priori_clipped = f'{os.path.splitext(a_priori_filename)[0]}_Subset_{strip_name}_Clipped{os.path.splitext(a_priori_filename)[1]}'
         strip_resampled = f'{tmp_dir}{strip_name}_Resampled_to_{a_priori_dem}{os.path.splitext(strip)[1]}'
         strip_resampled_intermediate = f'{tmp_dir}{strip_name}_Resampled_to_{intermediate_res}m{os.path.splitext(strip)[1]}'
+        strip_resampled_intermediate_nodata_removed = f'{tmp_dir}{strip_name}_Resampled_to_{intermediate_res}m_NoData_Removed{os.path.splitext(strip)[1]}'
         strip_resampled_intermediate_4326 = f'{tmp_dir}{strip_name}_Resampled_to_{intermediate_res}m_4326{os.path.splitext(strip)[1]}'
+        strip_ones = f'{tmp_dir}{strip_name}_Ones{os.path.splitext(strip)[1]}'
+        strip_ones_clipped = f'{tmp_dir}{strip_name}_Ones_Clipped{os.path.splitext(strip)[1]}'
+        strip_outline = f'{tmp_dir}{strip_name}_Outline.shp'
         strip_resampled_clipped = f'{tmp_dir}{strip_name}_Resampled_to_{a_priori_dem}_Clipped{os.path.splitext(strip)[1]}'
         diff_file = f'{os.path.splitext(a_priori_filename)[0]}_Minus_{strip_name}{os.path.splitext(strip)[1]}'
         #Subset a priori DEM to strip extents:
@@ -148,24 +154,45 @@ def main():
         #Resample strip to intermediate resolution, then warp to EPSG:4326, then resample to match a priori DEM:
         epsg_code = osr.SpatialReference(wkt=gdal.Open(strip,gdalconst.GA_ReadOnly).GetProjection()).GetAttrValue('AUTHORITY',1)
         intermediate_resample_command = f'gdalwarp -q -tr {intermediate_res} {intermediate_res} -r bilinear -of GTiff {warp_comp_bigtiff} {strip} {strip_resampled_intermediate}'
-        warp_4326_command = f'gdalwarp -q -s_srs EPSG:{epsg_code} -t_srs EPSG:4326 {warp_comp_bigtiff} {strip_resampled_intermediate} {strip_resampled_intermediate_4326}'
-        edit_command = f'gdal_edit.py -a_nodata 0 {strip_resampled}'
         subprocess.run(intermediate_resample_command,shell=True)
+        #Remove NoData values from intermediate resampled strip, but only on perimeter, not interior pixels:
+        src_strip_resampled_intermediate = gdal.Open(strip_resampled_intermediate,gdalconst.GA_ReadOnly)
+        ones_array = np.ones((src_strip_resampled_intermediate.RasterYSize,src_strip_resampled_intermediate.RasterXSize))
+        x_ones = np.linspace(src_strip_resampled_intermediate.GetGeoTransform()[0] + 0.5*src_strip_resampled_intermediate.GetGeoTransform()[1],
+                             src_strip_resampled_intermediate.GetGeoTransform()[0] - 0.5*src_strip_resampled_intermediate.GetGeoTransform()[1] + src_strip_resampled_intermediate.GetGeoTransform()[1]*src_strip_resampled_intermediate.RasterXSize,
+                             src_strip_resampled_intermediate.RasterXSize)
+        y_ones = np.linspace(src_strip_resampled_intermediate.GetGeoTransform()[3] + 0.5*src_strip_resampled_intermediate.GetGeoTransform()[5],
+                             src_strip_resampled_intermediate.GetGeoTransform()[3] - 0.5*src_strip_resampled_intermediate.GetGeoTransform()[5] + src_strip_resampled_intermediate.GetGeoTransform()[5]*src_strip_resampled_intermediate.RasterYSize,
+                             src_strip_resampled_intermediate.RasterYSize)
+        y_ones = np.flip(y_ones)
+        raster_to_geotiff(x_ones,y_ones,ones_array,epsg_code,strip_ones)
+        gdf_strip = get_strip_shp(strip_resampled_intermediate,tmp_dir)
+        mp_unary_union = shapely.ops.unary_union([shapely.geometry.Polygon(g) for g in gdf_strip.geometry.exterior])
+        gdf_strip_filtered = gpd.GeoDataFrame(pd.DataFrame({'strip':[strip]}),geometry=[mp_unary_union],crs='EPSG:32644')
+        gdf_strip_filtered.to_file(strip_outline)
+        ones_clip_command = f'gdalwarp -q -cutline {strip_outline} -cl {os.path.splitext(os.path.basename(strip_outline))[0]} -dstnodata 0 {strip_ones} {strip_ones_clipped}'
+        unset_nodata_command = f'gdal_edit.py -unsetnodata {strip_resampled_intermediate}'
+        multiply_command = f'gdal_calc.py --quiet -A {strip_resampled_intermediate} -B {strip_ones_clipped} --outfile={strip_resampled_intermediate_nodata_removed} --calc="A*B" {calc_comp_bigtiff}'
+        subprocess.run(ones_clip_command,shell=True)
+        subprocess.run(unset_nodata_command,shell=True)
+        subprocess.run(multiply_command,shell=True)
+        warp_4326_command = f'gdalwarp -q -s_srs EPSG:{epsg_code} -t_srs EPSG:4326 {warp_comp_bigtiff} {strip_resampled_intermediate_nodata_removed} {strip_resampled_intermediate_4326}'
+        edit_command = f'gdal_edit.py -a_nodata 0 {strip_resampled}'
         subprocess.run(warp_4326_command,shell=True)
-        resample_raster(strip_resampled_intermediate_4326,a_priori_subset,strip_resampled,resample_method='bilinear',compress=True,nodata=-9999,quiet_flag=True)
+        resample_raster(strip_resampled_intermediate_4326,a_priori_subset,strip_resampled,resample_method='bilinear',compress=True,nodata=0,quiet_flag=True)
         subprocess.run(edit_command,shell=True)
         #Clip a priori and strip to coastline:
-        a_priori_coastline_clip_command = f'gdalwarp -q -s_srs EPSG:4326 -t_srs EPSG:4326 -of GTiff -cutline {coastline_file} -cl {os.path.splitext(os.path.basename(coastline_file))[0]} -dstnodata -9999 {a_priori_subset} {a_priori_clipped} {warp_comp_bigtiff}'
-        strip_coastline_clip_command = f'gdalwarp -q -s_srs EPSG:4326 -t_srs EPSG:4326 -of GTiff -cutline {coastline_file} -cl {os.path.splitext(os.path.basename(coastline_file))[0]} -dstnodata -9999 {strip_resampled} {strip_resampled_clipped} {warp_comp_bigtiff}'
+        a_priori_coastline_clip_command = f'gdalwarp -q -s_srs EPSG:4326 -t_srs EPSG:4326 -of GTiff -cutline {coastline_file} -cl {os.path.splitext(os.path.basename(coastline_file))[0]} -dstnodata 0 {a_priori_subset} {a_priori_clipped} {warp_comp_bigtiff}'
+        strip_coastline_clip_command = f'gdalwarp -q -s_srs EPSG:4326 -t_srs EPSG:4326 -of GTiff -cutline {coastline_file} -cl {os.path.splitext(os.path.basename(coastline_file))[0]} -dstnodata 0 {strip_resampled} {strip_resampled_clipped} {warp_comp_bigtiff}'
         subprocess.run(a_priori_coastline_clip_command,shell=True)
         subprocess.run(strip_coastline_clip_command,shell=True)
         #Subtract a priori from strip:
-        calc_command = f'gdal_calc.py --quiet -A {a_priori_clipped} -B {strip_resampled_clipped} --calc="A-B" --outfile={diff_file} --NoDataValue=-9999 {calc_comp_bigtiff}'
+        calc_command = f'gdal_calc.py --quiet -A {a_priori_clipped} -B {strip_resampled_clipped} --calc="A-B" --outfile={diff_file} --NoDataValue=0 {calc_comp_bigtiff}'
         subprocess.run(calc_command,shell=True)
         #Load difference file and calculate statistics:
         src_diff = gdal.Open(diff_file,gdalconst.GA_ReadOnly)
         diff_array = np.asarray(src_diff.GetRasterBand(1).ReadAsArray())
-        diff_array[diff_array == -9999] = np.nan
+        diff_array[diff_array == 0] = np.nan
         pct_exceedance[i] = np.sum(np.abs(diff_array) > diff_threshold) / np.sum(~np.isnan(diff_array))
         if quiet_flag == False:
             print(f'{pct_exceedance[i]*100:.2f}% of pixels exceed {diff_threshold}m.')
@@ -177,7 +204,7 @@ def main():
             unclipped_array = np.asarray(src_unclipped.GetRasterBand(1).ReadAsArray())
             clipped_array = np.asarray(src_clipped.GetRasterBand(1).ReadAsArray())
             unclipped_array[unclipped_array == 0] = np.nan
-            clipped_array[clipped_array == -9999] = np.nan
+            clipped_array[clipped_array == 0] = np.nan
             pct_nan_unclipped = np.sum(np.isnan(unclipped_array)) / (unclipped_array.shape[0] * unclipped_array.shape[1])
             pct_nan_clipped = np.sum(np.isnan(clipped_array)) / (clipped_array.shape[0] * clipped_array.shape[1])
             pct_water[i] = pct_nan_clipped - pct_nan_unclipped
