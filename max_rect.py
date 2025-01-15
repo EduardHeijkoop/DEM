09a5bdf0166fdae0ef7d12ee36941ae904eb944e
Eduard Heijkoop <Eduard.Heijkoop@gmail.com>
2025-01-15 14:18:03 -0700

    Updated function to handle polygon as input

--- max_rect.py
+++ max_rect.py
@@ -1,60 +1,60 @@
 
 import numpy as np
 import cvxpy
-from shapely.geometry import Polygon
+# from shapely.geometry import Polygon
+import shapely
+from dem_utils import get_lonlat_geometry
+
+# def rect2poly(ll, ur):
+#     """
+#     Convert rectangle defined by lower left/upper right
+#     to a closed polygon representation.
+#     """
+#     x0, y0 = ll
+#     x1, y1 = ur
+
+#     return [
+#         [x0, y0],
+#         [x0, y1],
+#         [x1, y1],
+#         [x1, y0],
+#         [x0, y0]
+#     ]
 
 
-def rect2poly(ll, ur):
-    """
-    Convert rectangle defined by lower left/upper right
-    to a closed polygon representation.
-    """
-    x0, y0 = ll
-    x1, y1 = ur
+# def get_intersection(coords):
+#     """Given an input list of coordinates, find the intersection
+#     section of corner coordinates. Returns geojson of the
+#     interesection polygon.
+#     """
+#     ipoly = None
+#     for coord in coords:
+#         if ipoly is None:
+#             ipoly = shapely.geometry.Polygon(coord)
+#         else:
+#             tmp = shapely.geometry.Polygon(coord)
+#             ipoly = ipoly.intersection(tmp)
 
-    return [
-        [x0, y0],
-        [x0, y1],
-        [x1, y1],
-        [x1, y0],
-        [x0, y0]
-    ]
+#     # close the polygon loop by adding the first coordinate again
+#     first_x = ipoly.exterior.coords.xy[0][0]
+#     first_y = ipoly.exterior.coords.xy[1][0]
+#     ipoly.exterior.coords.xy[0].append(first_x)
+#     ipoly.exterior.coords.xy[1].append(first_y)
 
+#     inter_coords = zip(
+#         ipoly.exterior.coords.xy[0], ipoly.exterior.coords.xy[1])
 
-def get_intersection(coords):
-    """Given an input list of coordinates, find the intersection
-    section of corner coordinates. Returns geojson of the
-    interesection polygon.
-    """
-    ipoly = None
-    for coord in coords:
-        if ipoly is None:
-            ipoly = Polygon(coord)
-        else:
-            tmp = Polygon(coord)
-            ipoly = ipoly.intersection(tmp)
+#     inter_gj = {"geometry":
+#                 {"coordinates": [inter_coords],
+#                  "type": "Polygon"},
+#                 "properties": {}, "type": "Feature"}
 
-    # close the polygon loop by adding the first coordinate again
-    first_x = ipoly.exterior.coords.xy[0][0]
-    first_y = ipoly.exterior.coords.xy[1][0]
-    ipoly.exterior.coords.xy[0].append(first_x)
-    ipoly.exterior.coords.xy[1].append(first_y)
-
-    inter_coords = zip(
-        ipoly.exterior.coords.xy[0], ipoly.exterior.coords.xy[1])
-
-    inter_gj = {"geometry":
-                {"coordinates": [inter_coords],
-                 "type": "Polygon"},
-                "properties": {}, "type": "Feature"}
-
-    return inter_gj, inter_coords
+#     return inter_gj, inter_coords
 
 
 def two_pts_to_line(pt1, pt2):
     """
     Create a line from two points in form of
-
     a1(x) + a2(y) = b
     """
     pt1 = [float(p) for p in pt1]
@@ -66,7 +66,6 @@ def two_pts_to_line(pt1, pt2):
     a1 = -slp
     a2 = 1.
     b = -slp * pt1[0] + pt1[1]
-
     return a1, a2, b
 
 
@@ -76,7 +75,6 @@ def pts_to_leq(coords):
     x is of length 2 this is like A1(x1) + A2(x2) = B.
     returns A1, A2, B
     """
-
     A1 = []
     A2 = []
     B = []
@@ -90,29 +88,26 @@ def pts_to_leq(coords):
     return A1, A2, B
 
 
-def get_maximal_rectangle(coordinates):
+def get_maximal_rectangle(polygon:shapely.geometry.Polygon,buffer_val=0.001):
     """
     Find the largest, inscribed, axis-aligned rectangle.
-
     :param coordinates:
         A list of of [x, y] pairs describing a closed, convex polygon.
     Obtained from https://github.com/planetlabs/maxrect
     and edited with https://stackoverflow.com/questions/70586297/cvxpy-solvers-produce-solutions-of-different-shapes
     """
-
+    #Get lon/lat from shapely polygon
+    lon,lat = get_lonlat_geometry(polygon,append_nan=False)
+    coordinates = [[ln,lt] for ln,lt in zip(lon,lat)]
     coordinates = np.array(coordinates)
     x_range = np.max(coordinates, axis=0)[0]-np.min(coordinates, axis=0)[0]
     y_range = np.max(coordinates, axis=0)[1]-np.min(coordinates, axis=0)[1]
-
     scale = np.array([x_range, y_range])
     sc_coordinates = coordinates/scale
-
-    poly = Polygon(sc_coordinates)
+    poly = shapely.geometry.Polygon(sc_coordinates)
     inside_pt = (poly.representative_point().x,
                  poly.representative_point().y)
-
     A1, A2, B = pts_to_leq(sc_coordinates)
-
     bl = cvxpy.Variable(2)
     tr = cvxpy.Variable(2)
     br = cvxpy.Variable(2)
@@ -123,26 +118,20 @@ def get_maximal_rectangle(coordinates):
                    tl[1] == tr[1],
                    bl[1] == br[1],
                    ]
-
     for i in range(len(B)):
         if inside_pt[0] * A1[i] + inside_pt[1] * A2[i] <= B[i]:
             constraints.append(bl[0] * A1[i] + bl[1] * A2[i] <= B[i])
             constraints.append(tr[0] * A1[i] + tr[1] * A2[i] <= B[i])
             constraints.append(br[0] * A1[i] + br[1] * A2[i] <= B[i])
             constraints.append(tl[0] * A1[i] + tl[1] * A2[i] <= B[i])
-
         else:
             constraints.append(bl[0] * A1[i] + bl[1] * A2[i] >= B[i])
             constraints.append(tr[0] * A1[i] + tr[1] * A2[i] >= B[i])
             constraints.append(br[0] * A1[i] + br[1] * A2[i] >= B[i])
             constraints.append(tl[0] * A1[i] + tl[1] * A2[i] >= B[i])
-
     prob = cvxpy.Problem(obj, constraints)
     # prob.solve(solver=cvxpy.CVXOPT, verbose=False, max_iters=1000, reltol=1e-9)
     prob.solve()
-
     bottom_left = np.array(bl.value).T * scale
     top_right = np.array(tr.value).T * scale
-
-    # return list(bottom_left[0]), list(top_right[0])
-    return bottom_left, top_right
+    return bottom_left[0]+buffer_val,bottom_left[1]+buffer_val,top_right[0]-buffer_val,top_right[1]-buffer_val
